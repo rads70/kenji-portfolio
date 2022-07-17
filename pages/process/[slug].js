5a376129e5cc56e9fd60978e6a76d50f3e160658
Richard Suttie <richard@suttie.no>
2022-07-17 13:30:53 +0200

    Changed back to static props revalidate

--- pages/process/[slug].js
+++ pages/process/[slug].js
@@ -154,7 +154,7 @@ function filterDataToSingleItem(data, preview) {
 }
 
 export async function getStaticPaths() {
-   const query = groq`*[_type == "works"][].slug.current`;
+   const query = groq`*[_type == "process"][].slug.current`;
    const slugs = await getClient().fetch(query);
 
    const paths = slugs.map((slug) => `/process/${slug}`);
