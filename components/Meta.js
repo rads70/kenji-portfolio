907e157dc6e9e655d400dbccb910adc3ca215afe
Richard Suttie <richard@suttie.no>
2022-07-17 11:32:07 +0200

    Serverside props

--- components/Meta.js
+++ components/Meta.js
@@ -4,7 +4,7 @@ function Meta({ title, description, keywords }) {
    return (
       <Head>
          <meta charset='UTF-8' />
-         <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+         <meta http-Equiv='X-UA-Compatible' content='IE=edge' />
          <meta
             name='viewport'
             content='width=device-width, initial-scale=1.0'
